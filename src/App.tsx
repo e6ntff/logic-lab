b519e4a9451d755cbf0eaaf3b1aef2d5ac8f2280
e6ntff <pashatar2@gmail.com>
2024-04-27 14:39:43 +0300

    added flow to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import ReactFlow, { Background, ConnectionMode, Controls } from 'reactflow';
+import 'reactflow/dist/style.css';
+import { edgeTypes, nodeTypes } from './utils/types';
+import appStore from './utils/appStore';
 
 const App: React.FC = observer(() => {
-	return <></>;
+	const { nodes, edges, updateNodes, updateEdges, updateConnections } =
+		appStore;
+
+	return (
+		<ReactFlow
+			connectionMode={'loose' as ConnectionMode}
+			nodeTypes={nodeTypes}
+			edgeTypes={edgeTypes}
+			onNodesChange={updateNodes}
+			onEdgesChange={updateEdges}
+			onConnect={updateConnections}
+			nodes={nodes}
+			edges={edges}
+		>
+			<Background />
+			<Controls />
+		</ReactFlow>
+	);
 });
 
 export default App;
