8280d43979effa098a7077c9b8c5351e5c990dd4
e6ntff <pashatar2@gmail.com>
2024-04-27 18:03:11 +0300

    added block styles

--- src/utils/blockStyles.ts
+++ src/utils/blockStyles.ts
@@ -1,12 +1,37 @@
 import { CSSProperties } from 'react';
 
-export const AndStyle: CSSProperties = {
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
+export const connectorStyle: CSSProperties = {
+	blockSize: 15,
+	inlineSize: 20,
+	borderRadius: 3,
+	border: 0,
+	boxShadow: 'none',
+	background: '#000',
+};
+
+export const andStyle: CSSProperties = {
 	background: '#f00',
+	border: '3px solid #000',
 	borderRadius: 10,
 	padding: 10,
 	width: 100,
-	height: 200,
+	height: 150,
+};
+
+export const orStyle: CSSProperties = {
+	background: '#0f0',
+	border: '3px solid #000',
+	borderRadius: 10,
+	padding: 10,
+	width: 100,
+	height: 150,
+};
+
+export const notStyle: CSSProperties = {
+	background: '#fff',
+	border: '3px solid #000',
+	borderRadius: 10,
+	padding: 10,
+	width: 100,
+	height: 50,
 };
