c862d706bf211c6fca42d4acd2c2227e36f47f70
e6ntff <pashatar2@gmail.com>
2024-04-27 14:39:52 +0300

    added and block

--- src/components/And.tsx
+++ src/components/And.tsx
@@ -1,8 +1,36 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import { Handle, Position } from 'reactflow';
+import { AndStyle } from '../utils/blockStyles';
+import Title from 'antd/es/typography/Title';
 
-const And: React.FC = observer(() => {
-	return <></>;
+interface Props {
+	data: any;
+}
+
+const And: React.FC<Props> = observer(({ data }) => {
+	return (
+		<div style={AndStyle}>
+			<Title style={{ color: '#fff' }}>{data.label}</Title>
+			<Handle
+				id='a'
+				type='source'
+				position={'left' as Position}
+				style={{ background: '#555', top: '33.3%', bottom: '33.3%' }}
+			/>
+			<Handle
+				id='b'
+				type='source'
+				position={'left' as Position}
+				style={{ background: '#555', top: '75%', bottom: '25%' }}
+			/>
+			<Handle
+				id='c'
+				type='target'
+				position={'right' as Position}
+				style={{ background: '#555', top: '50%' }}
+			/>
+		</div>
+	);
 });
 
 export default And;
