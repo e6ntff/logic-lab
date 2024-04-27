26545c3c76377cfffe85cbafd652b41c36ec7ef0
e6ntff <pashatar2@gmail.com>
2024-04-27 14:40:13 +0300

    added store

--- src/utils/appStore.ts
+++ src/utils/appStore.ts
@@ -1,7 +1,61 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, reaction } from 'mobx';
+import {
+	Edge,
+	Node,
+	addEdge,
+	applyEdgeChanges,
+	applyNodeChanges,
+} from 'reactflow';
+import uniqid from 'uniqid';
+
+// nodes: Node<any, string | undefined>[] = [
+// 	{
+// 		id: '1',
+// 		position: { x: 0, y: 0 },
+// 		data: { label: 'Hello' },
+// 	},
+// 	{
+// 		id: '2',
+// 		position: { x: 100, y: 100 },
+// 		data: { label: 'World' },
+// 	},
+// 	{
+// 		id: '3',
+// 		position: { x: 200, y: 200 },
+// 		data: { label: 'AND' },
+// 		type: 'and',
+// 	},
+// ];
+// edges: Edge<any>[] = [{ id: '1-2', type: 'wire', source: '1', target: '2' }];
 
 class AppStore {
-	blocks = {};
+	nodes: Node<any, string | undefined>[] = JSON.parse(
+		localStorage.getItem('nodes') || '[]'
+	);
+	edges: Edge<any>[] = JSON.parse(localStorage.getItem('edges') || '[]');
+
+	updateNodes = (changes: any) => {
+		this.nodes = applyNodeChanges(changes, this.nodes);
+	};
+
+	updateEdges = (changes: any) => {
+		this.edges = applyEdgeChanges(changes, this.edges);
+	};
+
+	updateConnections = (changes: any) => {
+		this.edges = addEdge(changes, this.edges);
+	};
+
+	addNode = () => {
+		const node = {
+			id: uniqid,
+			position: { x: 200, y: 200 },
+			data: { label: 'AND' },
+			type: 'and',
+		};
+
+		this.nodes = [...this.nodes];
+	};
 
 	constructor() {
 		makeAutoObservable(this);
@@ -11,3 +65,17 @@ class AppStore {
 const appStore = new AppStore();
 
 export default appStore;
+
+reaction(
+	() => appStore.nodes,
+	() => {
+		localStorage.setItem('nodes', JSON.stringify(appStore.nodes));
+	}
+);
+
+reaction(
+	() => appStore.edges,
+	() => {
+		localStorage.setItem('edges', JSON.stringify(appStore.edges));
+	}
+);
