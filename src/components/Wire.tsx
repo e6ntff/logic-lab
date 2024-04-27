b1c8e41be70623dd55b8a8e65c5354f07d8fc6c9
e6ntff <pashatar2@gmail.com>
2024-04-27 18:02:51 +0300

    chandes wire style

--- src/components/Wire.tsx
+++ src/components/Wire.tsx
@@ -1,19 +1,48 @@
-import { observer } from "mobx-react-lite";
+import { CloseOutlined } from '@ant-design/icons';
+import { observer } from 'mobx-react-lite';
+import { useCallback } from 'react';
+import { EdgeLabelRenderer, getSmoothStepPath } from 'reactflow';
+import appStore from '../utils/appStore';
 
 const Wire: React.FC = observer(
 	({ id, sourceX, sourceY, targetX, targetY }: any) => {
+		const { removeEdge } = appStore;
+
+		const [edgePath, labelX, labelY] = getSmoothStepPath({
+			sourceX,
+			sourceY,
+			targetX,
+			targetY,
+		});
+
+		const handleRemoving = useCallback(() => {
+			removeEdge(id);
+		}, [id, removeEdge]);
+
 		return (
-			<g>
-				<path
-					id={id}
-					d={`M${sourceX},${sourceY}L${targetX},${targetY}`}
-					stroke='#555'
-					strokeWidth={2}
-					markerEnd='url(#marker-circle)'
-				/>
-			</g>
+			<>
+				<g>
+					<path
+						d={edgePath}
+						stroke='#000'
+						fill='#0000'
+						strokeWidth={3}
+						className='animated'
+					/>
+				</g>
+				<EdgeLabelRenderer>
+					<CloseOutlined
+						onClick={handleRemoving}
+						style={{
+							position: 'absolute',
+							transform: `translate(-50%, -50%) translate(${labelX}px, ${labelY}px)`,
+							pointerEvents: 'all',
+						}}
+					/>
+				</EdgeLabelRenderer>
+			</>
 		);
 	}
 );
 
-export default Wire
+export default Wire;
