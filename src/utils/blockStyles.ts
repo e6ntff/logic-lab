50fd5893ba513d71e5b673b6db560fc08bb84f0a
e6ntff <pashatar2@gmail.com>
2024-04-27 14:40:01 +0300

    added utils

---
+++ src/utils/blockStyles.ts
@@ -0,0 +1,12 @@
+import { CSSProperties } from 'react';
+
+export const AndStyle: CSSProperties = {
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	background: '#f00',
+	borderRadius: 10,
+	padding: 10,
+	width: 100,
+	height: 200,
+};
---
+++ src/utils/types.ts
@@ -0,0 +1,7 @@
+import { EdgeTypes, NodeTypes } from 'reactflow';
+import And from '../components/And';
+import Wire from '../components/Wire';
+
+export const nodeTypes: NodeTypes = { and: And };
+
+export const edgeTypes: EdgeTypes = { wire: Wire };
