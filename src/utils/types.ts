c5e30434f01b8b4caf201ba4dadd33304b6d1016
e6ntff <pashatar2@gmail.com>
2024-04-27 18:03:15 +0300

    added types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,13 @@
 import { EdgeTypes, NodeTypes } from 'reactflow';
 import And from '../components/And';
 import Wire from '../components/Wire';
+import Or from '../components/Or';
+import Not from '../components/Not';
 
-export const nodeTypes: NodeTypes = { and: And };
+export type nodeType = 'and' | 'or' | 'not';
+
+export type edgeType = 'wire';
+
+export const nodeTypes: NodeTypes = { and: And, or: Or, not: Not };
 
 export const edgeTypes: EdgeTypes = { wire: Wire };
