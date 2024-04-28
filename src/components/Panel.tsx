88cbb4791a3c5786ad08be9de2b05889e478bf19
e6ntff <pashatar2@gmail.com>
2024-04-28 21:35:00 +0300

    added nodes to panel

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -33,10 +33,10 @@ const Panel: React.FC = observer(() => {
 			<Button onClick={() => addNode('splitter')}>
 				<ShareAltOutlined />
 			</Button>
-			<Button onClick={() => addNode('flasher')}>
+			<Button onClick={() => addNode('flasher', 1000)}>
 				<HistoryOutlined />
 			</Button>
-			<Button onClick={() => addNode('delay')}>
+			<Button onClick={() => addNode('delay', 1000)}>
 				<EllipsisOutlined />
 			</Button>
 			<Button onClick={() => addNode('end')}>
