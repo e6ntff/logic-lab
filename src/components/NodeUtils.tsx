29ebc3c9ab04cbd76e83a277fe7e2c6b25e18f33
e6ntff <pashatar2@gmail.com>
2024-05-01 17:01:19 +0300

    added delete cross

--- src/components/NodeUtils.tsx
+++ src/components/NodeUtils.tsx
@@ -1,5 +1,6 @@
 import {
 	ArrowRightOutlined,
+	CloseOutlined,
 	RotateLeftOutlined,
 	RotateRightOutlined,
 } from '@ant-design/icons';
@@ -7,13 +8,14 @@ import { observer } from 'mobx-react-lite';
 import appStore from '../utils/appStore';
 import { useCallback, useMemo } from 'react';
 import { Node } from 'reactflow';
+import { Flex } from 'antd';
 
 interface Props {
 	id: string;
 }
 
 const NodeUtils: React.FC<Props> = observer(({ id }) => {
-	const { setNodeParameters, nodes } = appStore;
+	const { setNodeParameters, nodes, removeNode } = appStore;
 
 	const node: Node<any, string | undefined> | undefined = useMemo(
 		() => nodes.find((node: Node<any, string | undefined>) => node.id === id),
@@ -32,24 +34,41 @@ const NodeUtils: React.FC<Props> = observer(({ id }) => {
 	);
 
 	return (
-		<>
-			<ArrowRightOutlined
-				style={{
-					position: 'absolute',
-					top: 10,
-					left: 10,
-					rotate: `${rotation}deg`,
-				}}
-			/>
-			<RotateRightOutlined
-				onClick={() => handleNodeRotation(true)}
-				style={{ position: 'absolute', bottom: 10, right: 10 }}
-			/>
-			<RotateLeftOutlined
-				onClick={() => handleNodeRotation(false)}
-				style={{ position: 'absolute', bottom: 10, left: 10 }}
-			/>
-		</>
+		<Flex
+			vertical
+			style={{
+				position: 'absolute',
+				inset: 0,
+				inlineSize: '100%',
+				blockSize: '100%',
+				padding: 10,
+				boxSizing: 'border-box',
+				pointerEvents: 'none',
+			}}
+			justify='space-between'
+		>
+			<Flex justify='space-between'>
+				<ArrowRightOutlined
+					style={{
+						rotate: `${rotation}deg`,
+					}}
+				/>
+				<CloseOutlined
+					onClick={() => removeNode(id)}
+					style={{ pointerEvents: 'all' }}
+				/>
+			</Flex>
+			<Flex justify='space-between'>
+				<RotateLeftOutlined
+					onClick={() => handleNodeRotation(false)}
+					style={{ pointerEvents: 'all' }}
+				/>
+				<RotateRightOutlined
+					onClick={() => handleNodeRotation(true)}
+					style={{ pointerEvents: 'all' }}
+				/>
+			</Flex>
+		</Flex>
 	);
 });
 
